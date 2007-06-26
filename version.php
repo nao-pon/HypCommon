1b65fa7475bae5298af132b0cc7b1c81117db19c
nao-pon <hypweb@gmail.com>
2007-06-26 03:07:03 +0000

    Ver 20070626

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php
 /*
  * Created on 2007/05/30 by nao-pon http://hypweb.net/
- * $Id: version.php,v 1.4 2007/06/21 22:36:27 nao-pon Exp $
+ * $Id: version.php,v 1.5 2007/06/26 03:07:03 nao-pon Exp $
  */
 
-$version = '20070622';
+$version = '20070626';
 ?>
