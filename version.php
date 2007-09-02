48f99986fecbddacdac84858f1e9edf3c57f92b9
nao-pon <hypweb@gmail.com>
2007-09-02 02:28:53 +0000

    Supported nothing mbstring system.

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php
 /*
  * Created on 2007/05/30 by nao-pon http://hypweb.net/
- * $Id: version.php,v 1.10 2007/08/08 05:15:58 nao-pon Exp $
+ * $Id: version.php,v 1.11 2007/09/02 02:28:53 nao-pon Exp $
  */
 
-$version = '20070808';
+$version = '20070902';
 ?>
