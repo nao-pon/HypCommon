9a52770598b0ebdf86704b8caa7f7fb986070c99
nao-pon <nao-pon>
2007-12-06 04:08:03 +0000

    rewind to last version.

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php
 /*
  * Created on 2007/05/30 by nao-pon http://hypweb.net/
- * $Id: version.php,v 1.22 2007/12/05 12:13:24 nao-pon Exp $
+ * $Id: version.php,v 1.23 2007/12/06 04:09:06 nao-pon Exp $
  */
 
-$version = '20071205';
+$version = '20071130.1';
 ?>
