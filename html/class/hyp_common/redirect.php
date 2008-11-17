5b566457c179af6df392f4f69fa639e2df067f1a
nao-pon <hypweb@gmail.com>
2008-11-17 01:02:23 +0000

    Remove.

--- html/class/hyp_common/redirect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (isset($_GET['l'])) {
-	$url = $_GET['l'];
-	$google = 'http://www.google.co.jp/gwt/n?u=' . rawurlencode($url);
-	$url = str_replace('&amp;', '&',htmlspecialchars($_GET['l']));
-	header('Content-type: text/html; charset=Shift_JIS');
-	echo '<html><head><title>外部へ移動</title></head><body>外部サイトへ移動します。<br><br><a href="'.$url.'">'.$url.'</a><br><br><a href="'.$google.'">Google の携帯変換を使う</a></body></html>';
-}
