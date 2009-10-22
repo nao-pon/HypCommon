2fea2fa4b8d014934e52d521facc434434842b98
nao-pon <nao-pon>
2009-10-22 01:23:55 +0000

    Version 20091022.

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Created on 2007/05/30 by nao-pon http://hypweb.net/
- * $Id: version.php,v 1.106 2009/10/01 23:28:35 nao-pon Exp $
+ * $Id: version.php,v 1.107 2009/10/22 01:23:55 nao-pon Exp $
  */
 
-$version = '20091002';
+$version = '20091022';
