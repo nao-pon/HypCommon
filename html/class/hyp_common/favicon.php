9d4c58762d503b56fb7cbbf6a18d8f1c509ec1a7
nao-pon <hypweb@gmail.com>
2010-06-04 06:51:51 +0000

    Added contants 'PROTECTOR_SKIP_DOS_CHECK' & 'BIGUMBRELLA_DISABLED' for protector.

--- html/class/hyp_common/favicon.php
+++ html/class/hyp_common/favicon.php
@@ -1,11 +1,15 @@
 <?php
 /*
  * Created on 2008/02/11 by nao-pon http://hypweb.net/
- * $Id: favicon.php,v 1.1 2008/02/17 13:37:41 nao-pon Exp $
+ * $Id: favicon.php,v 1.2 2010/06/04 06:51:51 nao-pon Exp $
  */
 
 $xoopsOption['nocommon'] = TRUE;
 define('_LEGACY_PREVENT_LOAD_CORE_', TRUE);
+
+define('PROTECTOR_SKIP_DOS_CHECK', TRUE);
+define('BIGUMBRELLA_DISABLED', TRUE);
+
 include '../../mainfile.php' ;
 
 if( ! defined( 'XOOPS_TRUST_PATH' ) ) die( 'set XOOPS_TRUST_PATH in mainfile.php' ) ;
