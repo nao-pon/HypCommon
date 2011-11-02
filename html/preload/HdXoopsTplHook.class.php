c99111cfd3c786268ddb76a067a5ec14956a29dc
nao-pon <nao-pon>
2011-11-02 04:35:04 +0000

    Uses substr(XOOPS_URL, 7) as the prefix of complile_id.

--- html/preload/HdXoopsTplHook.class.php
+++ html/preload/HdXoopsTplHook.class.php
@@ -14,7 +14,7 @@ class HdXoopsTplHook extends XCube_ActionFilter
 		global $xoopsConfig ;
 
 		array_unshift( $xoopsTpl->plugins_dir , XOOPS_TRUST_PATH.'/libs/smartyplugins' ) ;
-		$compile_id = substr(XOOPS_URL, 4) . '-' . $xoopsConfig['template_set'] . '-' . $xoopsConfig['theme_set'] ;
+		$compile_id = substr(XOOPS_URL, 7) . '-' . $xoopsConfig['template_set'] . '-' . $xoopsConfig['theme_set'] ;
 		$xoopsTpl->compile_id = $compile_id ;
 		$xoopsTpl->_compile_id = $compile_id ;
 	}
